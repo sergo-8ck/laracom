ab786c36a41b7e6d569f475ced91aba23fe0879b
sergo-8ck <sergo-8ck@yandex.ru>
2019-10-11 21:54:36 +0300

    contact feedback form style

--- resources/views/frontend/articles/contact.blade.php
+++ resources/views/frontend/articles/contact.blade.php
@@ -29,67 +29,46 @@
                             <div class="contact_wrapper information_head">
                                 <h3 class="margin-bottom-25 margin-top-none">ФОРМА ОБРАТНОЙ
                                     СВЯЗИ</h3>
-                                <div class="form_contact margin-bottom-20">
-                                    <form action="" method="post" class="ajax_form af_example">
-
+                                <div class="bd-example">
+                                    <form action="{{ url('sendemail/send') }}" method="post" class="ajax_form af_example">
+                                        {{ csrf_field() }}
                                         <div class="form-group">
-                                            <div class="controls">
-                                                <input type="text" id="af_name" name="name" value=""
-                                                       placeholder="Имя" class="form-control">
-                                                <span class="error_name"></span>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <div class="controls">
-                                                <input type="email" id="af_email" name="email"
-                                                       value="" placeholder="Email"
-                                                       class="form-control">
-                                                <span class="error_email"></span>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="controls">
-                                                <input type="phone" id="af_phone" name="phone"
-                                                       value="" placeholder="Телефон"
-                                                       class="form-control">
-                                                <span class="error_phone"></span>
-                                            </div>
+                                            <label for="name">Имя</label>
+                                            <input type="text" id="af_name" name="name" value=""
+                                                   placeholder="Имя" class="form-control">
+                                            <span class="error_name"></span>
                                         </div>
 
                                         <div class="form-group">
-                                            <div class="controls">
-                                                <textarea id="af_message" name="message"
-                                                          class="form-control"
-                                                          placeholder="Сообщение"
-                                                          rows="5"></textarea>
-                                                <span class="error_message"></span>
-                                            </div>
-                                        </div>
-                                        <div class="g-recaptcha"
-                                             data-sitekey="6LdN0EUUAAAAAGsfoY9qsSFptQS--BEDXjo3zrye">
-                                            <div style="width: 304px; height: 78px;">
-                                                <textarea id="g-recaptcha-response"
-                                                          name="g-recaptcha-response"
-                                                          class="g-recaptcha-response"
-                                                          style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea>
-                                            </div>
+                                            <label for="email">Email</label>
+                                            <input type="email" id="af_email" name="email"
+                                                   value="" placeholder="Email"
+                                                   class="form-control">
+                                            <span class="error_email"></span>
                                         </div>
                                         <div class="form-group">
-                                            <div class="controls">
-                                                <button type="reset" class="btn btn-default">
-                                                    Очистить
-                                                </button>
-                                                <button type="submit" class="btn btn-danger">
-                                                    Отправить
-                                                </button>
-                                            </div>
+                                            <label for="phone">Телефон</label>
+                                            <input type="phone" id="af_phone" name="phone"
+                                                   value="" placeholder="Телефон"
+                                                   class="form-control">
+                                            <span class="error_phone"></span>
                                         </div>
-                                        <input type="hidden" name="af_action"
-                                               value="229d9aae02fecd48b5bcd5ad4022d472">
+
+                                        <div class="form-group">
+                                            <label for="message">Сообщение</label>
+                                            <textarea id="af_message" name="message"
+                                                      class="form-control"
+                                                      placeholder="Сообщение"
+                                                      rows="5"></textarea>
+                                            <span class="error_message"></span>
+                                        </div>
+                                        <div class="form-group">
+{{--                                                <button type="reset" class="btn btn-default">--}}
+{{--                                                    Очистить--}}
+{{--                                                </button>--}}
+                                            <input type="submit" name="send" class="btn btn-info">
+                                        </div>
                                     </form>
-
-
                                 </div>
                             </div>
                         </div>
